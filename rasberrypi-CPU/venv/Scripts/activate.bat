310bb48778e93703dc912d49e46213aa55a0df86
Rajanand <ultimatzanand@gmail.com>
2023-09-10 20:07:06 +0530

    venv

--- rasberrypi-CPU/venv/Scripts/activate.bat
+++ rasberrypi-CPU/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\projects\auto_chess\public\auto_chess\rasberrypi\venv
+set VIRTUAL_ENV=D:\projects\auto_chess\public\auto_chess\rasberrypi-CPU\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
