49ea9f2ef7fdcca7535c10b1fbfc6dd5e27fe882
Rajanand <ultimatzanand@gmail.com>
2023-09-11 09:55:51 +0530

    -

--- rasberrypi-CPU/main.py
+++ rasberrypi-CPU/main.py
@@ -1,7 +1,9 @@
 from blocks.pn532_chess_poss import possitions as piece_poss
 
-piece_poss.process.start()
+# piece_poss.process.start()
 
-while True:
-    a=input()
-    print(piece_poss.buff)
+piece_poss.thread_func()
+
+# while True:
+#     a=input()
+#     print(piece_poss.buff)
