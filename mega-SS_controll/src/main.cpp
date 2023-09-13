c5615907349eeb913983a4b5d8f4c7acfe0dac2b
Rajanand <ultimatzanand@gmail.com>
2023-09-13 22:32:55 +0530

    PN532 c++ parllel read solution
    incomplete code

--- mega-SS_controll/src/main.cpp
+++ mega-SS_controll/src/main.cpp
@@ -1,154 +1,181 @@
-#include <Arduino.h>
-#include <cmd.h>
+/**************************************************************************/
+/*!
+    @file     readMifare.pde
+    @author   Adafruit Industries
+	@license  BSD (see license.txt)
+
+    This example will wait for any ISO14443A card or tag, and
+    depending on the size of the UID will attempt to read from it.
+
+    If the card has a 4-byte UID it is probably a Mifare
+    Classic card, and the following steps are taken:
+
+    - Authenticate block 4 (the first block of Sector 1) using
+      the default KEYA of 0XFF 0XFF 0XFF 0XFF 0XFF 0XFF
+    - If authentication succeeds, we can then read any of the
+      4 blocks in that sector (though only block 4 is read here)
+
+    If the card has a 7-byte UID it is probably a Mifare
+    Ultralight card, and the 4 byte pages can be read directly.
+    Page 4 is read by default since this is the first 'general-
+    purpose' page on the tags.
+
+
+This is an example sketch for the Adafruit PN532 NFC/RFID breakout boards
+This library works with the Adafruit NFC breakout
+  ----> https://www.adafruit.com/products/364
+
+Check out the links above for our tutorials and wiring diagrams
+These chips use SPI or I2C to communicate.
+
+Adafruit invests time and resources providing this open source code,
+please support Adafruit and open-source hardware by purchasing
+products from Adafruit!
+
+*/
+/**************************************************************************/
+#include <Wire.h>
 #include <SPI.h>
-// #include <Adafruit_SPIDevice.h>
+#include <Adafruit_PN532.h>
 
-#define ss 2
+// If using the breakout with SPI, define the pins for SPI communication.
 
-// Adafruit_SPIDevice *spi_dev = new Adafruit_SPIDevice(ss, 13, 12, 11, 1000000, SPI_BITORDER_LSBFIRST, SPI_MODE0);
-// Adafruit_SPIDevice sp(ss, 1000000, SPI_BITORDER_LSBFIRST, SPI_MODE0);
+// #define PN532_SS   (2)
 
+// If using the breakout or shield with I2C, define just the pins connected
+// to the IRQ and reset lines.  Use the values below (2, 3) for the shield!
+// #define PN532_IRQ   (2)
+// #define PN532_RESET (3)  // Not connected by default on the NFC Shield
 
-byte packet_buffer [64];
+// Uncomment just _one_ line below depending on how your breakout or shield
+// is connected to the Arduino:
 
-uint16_t timeout = 20;
+// Use this line for a breakout with a software SPI connection (recommended):
+// Adafruit_PN532 nfc(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
 
-byte pn532ack[] = {0x00, 0x00, 0xFF, 0x00, 0xFF, 0x00};
+// Use this line for a breakout with a hardware SPI connection.  Note that
+// the PN532 SCK, MOSI, and MISO pins need to be connected to the Arduino's
+// hardware SPI SCK, MOSI, and MISO pins.  On an Arduino Uno these are
+// SCK = 13, MOSI = 11, MISO = 12.  The SS line can be any digital IO pin.
+Adafruit_PN532 nfc(2);
 
-//SPI functions
-  bool write(const uint8_t *buffer, int len){
-    Serial.print("func write()");
-    digitalWrite(ss,LOW);
-    // byte buff[len];
-    for (int i = 0; i < len; i++) {
-      // buff[i] = buffer[i];
-      SPI.transfer(buffer[i]);
-    }
-    // SPI.transfer(buff,len);
-    digitalWrite(ss,HIGH);
-    Serial.println("func write() end");
-    return true;
-  }
+// Or use this line for a breakout or shield with an I2C connection:
+//Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);
 
-  bool write_then_read(const uint8_t *write_buffer, size_t write_len, uint8_t *read_buffer, size_t read_len, uint8_t sendvalue=(0xFF)){
-    digitalWrite(ss,LOW);
-    //write
-    for (size_t i = 0; i < write_len; i++) {
-      SPI.transfer(write_buffer[i]);
-    }
-    //read
-    for (size_t i = 0; i < read_len; i++) {
-    read_buffer[i] = SPI.transfer(sendvalue);
-  }
-    digitalWrite(ss,HIGH);
+// Or use hardware Serial:
+//Adafruit_PN532 nfc(PN532_RESET, &Serial1);
 
-    return true;
-  }  
-//SPI functions
-
-void writecommand(uint8_t *cmd, int cmdlen)
-{
-  Serial.println("func writecommand()");
-  uint8_t checksum;
-  uint8_t packet[9 + cmdlen];
-  uint8_t *p = packet;
-  cmdlen++;
-
-  p[0] = PN532_SPI_DATAWRITE;
-  p++;
-  p[0] = PN532_PREAMBLE;
-  p++;
-  p[0] = PN532_STARTCODE1;
-  p++;
-  p[0] = PN532_STARTCODE2;
-  p++;
-  checksum = PN532_PREAMBLE + PN532_STARTCODE1 + PN532_STARTCODE2;
-  p[0] = cmdlen;
-  p++;
-  p[0] = ~cmdlen + 1;
-  p++;
-  p[0] = PN532_HOSTTOPN532;
-  p++;
-  checksum += PN532_HOSTTOPN532;
-
-  for (uint8_t i = 0; i < cmdlen - 1; i++)
-  {
-    p[0] = cmd[i];
-    p++;
-    checksum += cmd[i];
-  }
-
-  p[0] = ~checksum;
-  p++;
-  p[0] = PN532_POSTAMBLE;
-  p++;
-
-  write(packet, 8+cmdlen);
-}
-
-bool readack(){
-  uint8_t ackbuff[6];
-  uint8_t cmd = PN532_SPI_DATAREAD;
-  write_then_read(&cmd, 1, ackbuff, 6);
-  Serial.println(ackbuff[0]);
-  return (0 == memcmp((char *)ackbuff, (char *)pn532ack, 6));
-}
-
-bool isready()
-{
-  uint8_t cmd = PN532_SPI_STATREAD;
-  uint8_t reply;
-  write_then_read(&cmd, 1, &reply, 1);
-  return reply == PN532_SPI_READY;
-}
-
-bool waitready(uint16_t timeout)
-{
-  while (!isready())
-  {
-    if (timeout != 0)
-    {
-      timeout--;
-    }else
-     return false;
-    Serial.println("getting ACK!");
-    delay(1);
-  }
-  Serial.println("foud ACK!");
-  return true;
-}
-
-bool checkACK(uint8_t *cmd, uint8_t cmdlen, uint16_t timeout=1000){
-  Serial.println("func checkACK()");
-  writecommand(cmd, cmdlen);
-  if (!waitready(timeout)) {
-    return false;
-  }
-  if (!readack()) {
-    return false;
-  }
-
-  return true;
-}
-
-
-void setup(void)
-{
+void setup(void) {
   Serial.begin(9600);
-  Serial.println("System initialized");
-  pinMode(ss,OUTPUT);
-  digitalWrite(ss,HIGH);
-  SPI.begin();
-  
-  SPI.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE0));
-  delay(3000);
-  packet_buffer[0] = PN532_COMMAND_GETFIRMWAREVERSION;
-  Serial.println(checkACK(packet_buffer,1));
-  SPI.endTransaction();
-  Serial.println("copleted.");
+  while (!Serial) delay(10); // for Leonardo/Micro/Zero
+
+  Serial.println("Hello!");
+
+  nfc.begin();
+
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (! versiondata) {
+    Serial.print("Didn't find PN53x board");
+    while (1); // halt
+  }
+  // Got ok data, print it out!
+  Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX);
+  Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC);
+  Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
+
+  Serial.println("Waiting for an ISO14443A Card ...");
 }
 
-void loop()
-{
-  delay(300);
+
+void loop(void) {
+  // uint8_t success;
+  // uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+  // uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+
+  // // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
+  // // 'uid' will be populated with the UID, and uidLength will indicate
+  // // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
+  // success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+
+  // if (success) {
+  //   // Display some basic information about the card
+  //   Serial.println("Found an ISO14443A card");
+  //   Serial.print("  UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
+  //   Serial.print("  UID Value: ");
+  //   nfc.PrintHex(uid, uidLength);
+  //   Serial.println("");
+
+  //   if (uidLength == 4)
+  //   {
+  //     // We probably have a Mifare Classic card ...
+  //     Serial.println("Seems to be a Mifare Classic card (4 byte UID)");
+
+  //     // Now we need to try to authenticate it for read/write access
+  //     // Try with the factory default KeyA: 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF
+  //     Serial.println("Trying to authenticate block 4 with default KEYA value");
+  //     uint8_t keya[6] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+
+	//   // Start with block 4 (the first block of sector 1) since sector 0
+	//   // contains the manufacturer data and it's probably better just
+	//   // to leave it alone unless you know what you're doing
+  //     success = nfc.mifareclassic_AuthenticateBlock(uid, uidLength, 4, 0, keya);
+
+  //     if (success)
+  //     {
+  //       Serial.println("Sector 1 (Blocks 4..7) has been authenticated");
+  //       uint8_t data[16];
+
+  //       // If you want to write something to block 4 to test with, uncomment
+	// 	// the following line and this text should be read back in a minute
+  //       //memcpy(data, (const uint8_t[]){ 'a', 'd', 'a', 'f', 'r', 'u', 'i', 't', '.', 'c', 'o', 'm', 0, 0, 0, 0 }, sizeof data);
+  //       // success = nfc.mifareclassic_WriteDataBlock (4, data);
+
+  //       // Try to read the contents of block 4
+  //       success = nfc.mifareclassic_ReadDataBlock(4, data);
+
+  //       if (success)
+  //       {
+  //         // Data seems to have been read ... spit it out
+  //         Serial.println("Reading Block 4:");
+  //         nfc.PrintHexChar(data, 16);
+  //         Serial.println("");
+
+  //         // Wait a bit before reading the card again
+  //         delay(1000);
+  //       }
+  //       else
+  //       {
+  //         Serial.println("Ooops ... unable to read the requested block.  Try another key?");
+  //       }
+  //     }
+  //     else
+  //     {
+  //       Serial.println("Ooops ... authentication failed: Try another key?");
+  //     }
+  //   }
+
+  //   if (uidLength == 7)
+  //   {
+  //     // We probably have a Mifare Ultralight card ...
+  //     Serial.println("Seems to be a Mifare Ultralight tag (7 byte UID)");
+
+  //     // Try to read the first general-purpose user page (#4)
+  //     Serial.println("Reading page 4");
+  //     uint8_t data[32];
+  //     success = nfc.mifareultralight_ReadPage (4, data);
+  //     if (success)
+  //     {
+  //       // Data seems to have been read ... spit it out
+  //       nfc.PrintHexChar(data, 4);
+  //       Serial.println("");
+
+  //       // Wait a bit before reading the card again
+  //       delay(1000);
+  //     }
+  //     else
+  //     {
+  //       Serial.println("Ooops ... unable to read the requested page!?");
+  //     }
+  //   }
+  // }
 }
